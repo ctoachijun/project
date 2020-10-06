a4bb5ca9092361e7a4c7e7648690fbf48001e9bd
Jang <ctoachijun@naver.com>
2020-10-06 11:45:45 +0900

    기존 옵션값을 hidden으로 넘겨 하나로 정리해 DB에 추가할수있게 변경

--- www/theme/cookie/mobile/app/add_routine_option.php
+++ www/theme/cookie/mobile/app/add_routine_option.php
@@ -22,26 +22,31 @@
 <!-- <link rel="stylesheet" href="//resources/demos/style.css"> -->
 <link rel="stylesheet" href="./style.css">
 <div class="rc_option_box">
-  <?while($row =sql_fetch_array($rso)){?>
-    <?
-//    if(!strpos($rs['rc_option'],$row['opt_id'])){
-//  strpos값이 0일때 1로 바꿈
-    $jud = strpos($rs['rc_option'],$row['opt_id']);
-    if($jud==0){
-      $jud++;
-    }
-    if(!$jud){
-    ?>
-      <p class="options">
-        <label>
-          <input type="checkbox" value="<?=$row['opt_id']?>" name="opt_id[]">
-          <?=$row['opt_name']?>
-          <?=number_format($row['opt_price_1'])?>원
-        </label>
-      </p>
+  <form action="./proc.php" method="post" accept-charset="utf-8" enctype="multipart/form-data">
+  <!-- <form action="./add_routine_option.php" method="post" accept-charset="utf-8" enctype="multipart/form-data"> -->
+    <input type="hidden" name="work_mode" value="update_routine_option">
+    <input type="hidden" name="rc_idx" value="<?=$rc_idx?>">
+    <?while($row =sql_fetch_array($rso)){
+  //  if(!strpos($rs['rc_option'],$row['opt_id'])){
+  //  strpos값이 0일때 1로 바꿈
+        $jud = strpos($rs['rc_option'],$row['opt_id']);
+        if($jud==0){
+          $jud++;
+        }
+        if(!$jud){
+        ?>
+          <p class="options">
+            <label>
+              <input type="checkbox" value="<?=$row['opt_id']?>" name="opt_id[]">
+              <?=$row['opt_name']?>
+              <?=number_format($row['opt_price_1'])?>원
+            </label>
+          </p>
+      <?}else{?>
+          <input type="hidden" name="opt_id[]" value="<?=$row['opt_id']?>">
+      <?}?>
     <?}?>
-  <?}?>
-  <button type="submit" onclick="okimp()" class="wa_bnt">추가옵션결제</button>
+    <button type="submit" onclick="okimp()" class="wa_bnt">추가옵션결제</button>
 </div>
 <script>
 
